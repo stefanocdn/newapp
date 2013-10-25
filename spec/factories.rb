c068ad7f81c90ceae2e355b75bddb91ead52cf1b
stephane cedroni <stephane.cedroni@gmail.com>
2013-10-25 19:39:10 -0400

    Add auth and rspec

---
+++ spec/factories.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:first_name)  { |n| "FirstName #{n}" }
+    sequence(:last_name)  { |n| "LastName #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
+end
